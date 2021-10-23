d7e508fe5e7cc1e9318a817672b2005663d20c3e
elifnurkarakoc <elifnurkarakoc@gmail.com>
2021-10-23 22:04:26 +0300

    :white_check_mark: update popup form validation error message

--- src/components/TodoPopUp/validations.ts
+++ src/components/TodoPopUp/validations.ts
@@ -3,10 +3,10 @@ import * as yup from "yup";
 const max = 50;
 const min = 5;
 export const validationSchema = yup.object().shape({
-    title: yup.string().required().min(min).max(max),
-    date: yup.date().required("Please select date").min(new Date(), "You can't choose a past date!"),
-    tag: yup.string().required(),
-    description: yup.string().required(),
+    title: yup.string().required("Title is the required field").min(min).max(max),
+    date: yup.date().nullable().typeError('Invalid Date').required("Date is the required field").min(new Date(), "You can't choose a past date!"),//yup.date().required("Date is the required field").min(new Date(), "You can't choose a past date!"),
+    tag: yup.string().required("Tag must be selected"),
+    description: yup.string().required("Description is the required field"),
     important: yup.boolean(),
     completed: yup.boolean(),
     deleted: yup.boolean(),
